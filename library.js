fac18868d2f7a1765458a515d2b9f7b3254bca58
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-08-08 18:28:59 +0100

    Create Book object & show books function

--- library.js
+++ library.js
@@ -1,13 +1,50 @@
+let library = [];
+
+// Book object
+
 function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
 
     this.info = function() {
-        return (`${title}, ${author}, ${pages}`);
-    }
+        return `${this.title}, ${this.author}, ${this.pages}`;
+    };
 }
 
-const colourOfMagic = new Book ('The Colour of Magic', 'Sir Terry Pratchett', 500);
+function addBookToLibrary(title, author, pages) {
+    const newBook = new Book(title, author, pages);
+    library.push(newBook);
+}
 
-console.log(colourOfMagic.info());
+
+// Test creating new book and returning info
+//const colourOfMagic = new Book ('The Colour of Magic', 'Sir Terry Pratchett', 500);
+
+//console.log(colourOfMagic.info());
+
+// Test adding new book to library
+
+addBookToLibrary('The Colour of Magic', 'Sir Terry Pratchett', 500);
+
+addBookToLibrary('The Light Fantastic', 'Sir Terry Pratchett', 450);
+
+console.log(library);
+
+function showBooks() {
+    const bookContainer = document.querySelector('.book-container');
+    bookContainer.innerHTML = '';
+
+    library.forEach((book) => {
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
+
+        const bookInfo = document.createElement('p');
+        bookInfo.textContent = book.info();
+
+        bookCard.appendChild(bookInfo);
+        bookContainer.appendChild(bookCard);
+    })
+}
+
+showBooks();
