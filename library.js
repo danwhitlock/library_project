efb3abe200ba6c1e98bd674907af7630633717ae
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-08-10 18:01:33 +0100

    Add listeners and functionality to handle form submission

--- library.js
+++ library.js
@@ -63,3 +63,22 @@ newBookButton.addEventListener('click', () => {
     bookForm.style.display = bookForm.style.display === 'block' ? 'none' : block;
 });
 
+// Listener for form submission
+
+bookForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // prevent default submission behaviour
+
+    // retrieve values from form 
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read').checked;  // boolean if box is checked or not
+
+    addBookToLibrary(title, author, pages, read); // add to inventory
+    showBooks(); // show updated inventory
+
+    // clear the form
+    bookForm.reset();
+    bookForm.style.display = 'none';
+});
+
